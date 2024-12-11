2829d6bd26d804e0b577bd0f17bb218f08ac41e3
Randy <randykeller1193@gmail.com>
2024-12-11 14:28:27 -0600

    add ramllah and keyna to users for admin panel

--- server/users.js
+++ server/users.js
@@ -3,6 +3,14 @@ const users = [
     username: "randy",
     password: "!1Stocksaretight",
   },
+  {
+    username: "ramallah",
+    password: "NucoordAtlas",
+  },
+  {
+    username: "keyna",
+    password: "NucoordAtlas",
+  },
 ];
 
 module.exports = { users };
